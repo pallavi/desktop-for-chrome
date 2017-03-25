f93ce8e91874baf2a77c6ba9f95bfd3507f73e00
Pallavi Shankar <pallavishankar6@gmail.com>
2017-03-25 00:05:28 -0400

    Added am/pm to clock

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# chrome-theme
+# Desktop for Chrome
+A Google Chrome extension that turns the new tab page into a desktop workstation.
+
+## Instructions
+
+## Sources
+Current weather information is from the OpenWeatherMap API. Calendar events are from the Google Calendar API and use the Google account of whoever is currently logged into Chrome. The world map and all logos for website icons are adapted from Wikimedia Commons (except for the Google Keep logo, which was recreated on Adobe Illustrator for this project).
--- src/js/clock.js
+++ src/js/clock.js
@@ -3,6 +3,7 @@ function startTime() {
   const hours = convertToStandardHours(today.getHours());
   const minutes = addZeroes(today.getMinutes());
   document.getElementById('time').innerHTML = hours + ':' + minutes;
+  setTimePeriod(today.getHours());
   setTimeout(startTime, 5000);
 }
 
@@ -11,6 +12,14 @@ function convertToStandardHours(hrs) {
     return 12;
   }
   return (hrs > 12 ? hrs - 12 : hrs);
+}
+
+function setTimePeriod(hrs) {
+  if (hrs < 12) {
+    document.getElementById('timeperiod').innerHTML = 'am';
+  } else {
+    document.getElementById('timeperiod').innerHTML = 'pm';
+  }
 }
 
 function addZeroes(min) {
